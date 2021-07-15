162015b473e34222bd90e2df037e0c27344d4308
Suzan-Elmajali <74111783+Suzan-Elmajali@users.noreply.github.com>
2021-07-15 21:16:05 +0300

    Manage Users

--- app/routes/content_set.py
+++ app/routes/content_set.py
@@ -73,62 +73,48 @@ def prepare_content_object(new_id, content_csv_obj):
 # def edit(id):
     #  return render_template('edit.html', title='Edit content set')
 
-@content_set.route('/edit_content_set/', methods=['GET','POST'])
-def edit_content():
+@content_set.route('/edit_content_set/edit_content/<int:id>', methods=['GET','POST'])
+def edit_content(id):
     if current_user.is_authenticated:
-
-    
-        # http://127.0.0.1:5000/content_set/edit_content_set/?id=123
-
-        # # fetch Id from request parameter, if not given, defaults to 1 for testing purposes now
-
-        # print('ans for update ', request.args.get('id'))
-        content_set_id = request.args.get('id') if request.args.get('id') is not None else 1
-
         # # fetch corresponding dataset from DB
-        
         if request.method == 'POST':
-            content_set_rec = db.session.get(ContentSet, {"id": content_set_id})
-
-            # render in UI for Edit modal
+            content_set_rec = db.session.get(ContentSet, {"id": id})
             return render_template('edit_content_set.html',
-                                
+                                id = id,
                                 location=content_set_rec.location,
                                 exported_on=content_set_rec.exproted_on,
                                 imported_on=content_set_rec.imported_on,
                                 imported_by=content_set_rec.imported_by,
                                 lib_version = content_set_rec.lib_version
                                 )
-            
         else:
-            # Handle from DB
+            return redirect(url_for('main.show'))
             
-            Exported_date = str(request.form['Exported_on'])
-            year, month, day = map(int, Exported_date.split('-'))
-            export_date = datetime.date(year, month, day)
-
-            Imported_date = str(request.form['Imported_on'])
-            year1, month1, day1 = map(int, Imported_date.split('-'))
-            import_date = datetime.date(year1, month1, day1)
-
-            content_set = ContentSet(location=request.form['Location'], exported_on=export_date, imported_on=import_date,
-                                    imported_by=request.form['Imported_by'], lib_version=request.form['Library_version'])
-
-            print('update now' ,content_set_id)
-            # Updating the content_set
-            value = ContentSet.query.filter_by(id=content_set_id).first()
-            
-            value.location = content_set.location
-            value.export_date = content_set.exproted_on
-            value.lib_version = content_set.lib_version
-            value.imported_by = content_set.imported_by
-            value.imported_on = content_set.imported_on
-
-            db.session.commit()                         
-
-            return render_template('edit_content_set.html')
-    else:
-        return render_template("user_login.html", title='login')
+@content_set.route('/edit_content_set/save_content/<int:id>', methods=['GET','POST'])
+def save_edited_content(id):
+    if request.method == 'POST':
+        # Handle from DB
+        Exported_date = str(request.form['Exported_on'])
+        year, month, day = map(int, Exported_date.split('-'))
+        export_date = datetime.date(year, month, day)
+        Imported_date = str(request.form['Imported_on'])
+        year1, month1, day1 = map(int, Imported_date.split('-'))
+        import_date = datetime.date(year1, month1, day1)
+        location = request.form['Location']
+        imported_by=request.form['Imported_by']
+        lib_version=request.form['Library_version']      
+        # Updating the content_set
+        value = ContentSet.query.filter_by(id=id).first()            
+        value.location = location
+        value.exproted_on = export_date
+        value.lib_version = lib_version
+        value.imported_by = imported_by
+        value.imported_on = import_date
+        db.session.commit()                         
+        return redirect(url_for('content_set.show_all'))
+    return redirect(url_for('content_set.show_all'))
+    
+        
 
 @content_set.route('/delete/<int:id1>', methods=['GET','POST'])
 def delete(id1):
